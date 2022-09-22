d52766f70420bfaba4e8c01d69ee0364c6412cc6
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-09-22 07:41:29 -0300

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
 
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+
   def last_three_posts
     Post.where(author: self).order(updated_at: :desc).first(3)
   end
