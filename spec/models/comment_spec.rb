5345c9be72ce678a126f50ad3d47e517c88d2fea
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-09-23 17:57:23 -0300

    Add tests for comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   # Create Post and users to add new comments
-  new_post = Post.create(title: 'My first post', author: User.create(name: 'Every One'))
-  other_user = User.create(name: 'Other Else')
-  subject { Comment.create(text: 'Hi this is a comment', author: other_user, post: new_post) }
+  new_post = Post.create(title: 'My first post', author: User.create(name: 'John Doe'))
+  other_user = User.create(name: 'Jane Smith')
+  subject { Comment.create(text: 'Hi comment', author: other_user, post: new_post) }
 
   it 'UpdatesCommentsCounter increments post comment_counter' do
     expect(subject.post.comments_counter).to be 1
-    Comment.create(text: 'Hi this is a 2nd comment', author: subject.author, post: subject.post)
+    Comment.create(text: 'Hi comment', author: subject.author, post: subject.post)
     expect(subject.post.comments_counter).to be 2
   end
 end
