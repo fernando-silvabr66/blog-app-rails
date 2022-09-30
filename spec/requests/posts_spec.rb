6c87adbf4bb4ef685f018fed1a757cdd26a788fb
Fernando Silva <fernando.silvabr66@gmail.com>
2022-09-29 23:25:46 -0300

    Update tests for Post controller show action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,31 +1,38 @@
 require 'rails_helper'
-
 RSpec.describe 'Posts', type: :request do
   before(:each) do
     @user = User.create(name: 'David')
     @id = @user.id
   end
-
   describe 'GET /users/:id/posts/' do
     before(:example) { get "/users/#{@id}/posts/" }
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'renders the correct template' do
+      expect(response).to render_template(:index)
+    end
+    it 'includes the correct placeholder text in its body' do
+      expect(response.body).to include('<h1>Posts</h1>')
+    end
+  end
+
+  describe 'GET /users/:id/posts/:id/' do
+    before(:example) do
+      post = Post.create(title: 'My first post', author: @user, text: 'Text of my first post')
+      get "/users/#{@id}/posts/#{post.id}/"
+    end
 
     it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
     it 'renders the correct template' do
-      expect(response).to render_template(:index)
+      expect(response).to render_template(:show)
     end
 
     it 'includes the correct placeholder text in its body' do
-      expect(response.body).to include('<h1>Posts</h1>')
-    end
-  end
-
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
-      expect(response).to have_http_status(:success)
+      expect(response.body).to include('<div class="detailed_post">')
     end
   end
 end
