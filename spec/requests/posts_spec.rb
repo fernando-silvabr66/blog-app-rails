6438f9695269fc90caae307a7ce3e58a97f272d6
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-09-29 23:30:48 -0300

    Fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 RSpec.describe 'Posts', type: :request do
   before(:each) do
     @user = User.create(name: 'David')
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /' do
-    before(:example) { get '/'}
+    before(:example) { get '/' }
 
     it 'returns http success' do
       expect(response).to have_http_status(:success)
